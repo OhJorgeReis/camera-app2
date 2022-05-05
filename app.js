ed47b418f60086886a4ff727afa7fac44f405c59
Jorge Reis <43137845+OhJorgeReis@users.noreply.github.com>
2022-05-05 15:54:08 +0200

    draggable

--- app.js
+++ app.js
@@ -26,3 +26,33 @@ cameraTrigger.onclick = function () {
 };
 
 window.addEventListener("load", cameraStart, false);
+
+interact(".draggable").draggable({
+  // enable inertial throwing
+  inertia: true,
+  // keep the element within the area of it's parent
+
+  // enable autoScroll
+  autoScroll: true,
+
+  listeners: {
+    move: dragMoveListener,
+  },
+});
+
+function dragMoveListener(event) {
+  var target = event.target;
+  // keep the dragged position in the data-x/data-y attributes
+  var x = (parseFloat(target.getAttribute("data-x")) || 0) + event.dx;
+  var y = (parseFloat(target.getAttribute("data-y")) || 0) + event.dy;
+
+  // translate the element
+  target.style.transform = "translate(" + x + "px, " + y + "px)";
+
+  // update the posiion attributes
+  target.setAttribute("data-x", x);
+  target.setAttribute("data-y", y);
+}
+
+// this function is used later in the resizing and gesture demos
+window.dragMoveListener = dragMoveListener;
